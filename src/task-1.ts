f641215c767b4ad556f723e2e9a1fcc2b50e2a02
jt <olex.bro92@gmail.com>
2025-07-21 22:18:39 +0200

    hw-ts-01

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const user: { name: string; age: number; isOnline: boolean } = {
+  name: "Alice",
+  age: 30,
+  isOnline: true,
+};
+
+console.log(`User: ${user.name}, Age: ${user.age}, Online: ${user.isOnline}`);
